9906ac4c42c917ca04b48b34aca3646d032a3c5f
dellt1 <tsinghmathoda@gmail.com>
2023-04-12 10:22:23 -0400

    v5

---
+++ ans4.py
@@ -0,0 +1,21 @@
+# a represents whole list
+a = []
+b = int(input("Enter Number: "))
+i = 0
+while (i < b):
+  #append is used for getting the position of specific number
+  a.append(int(input("Enter Element")))
+  i = i + 1
+print(a)
+
+c=[]
+i=0
+#for getting output greater than 12 
+while (i < b):
+  if(a[i] > 12):
+    c.append(a[i]) 
+  else:
+    pass
+ 
+  i = i + 1
+print("lists of number larger than 12",c)

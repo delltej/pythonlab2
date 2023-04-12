4aa4c0f73a927a06973fe99977ab46d4d9003e4b
dellt1 <tsinghmathoda@gmail.com>
2023-04-12 09:52:03 -0400

    v3

--- ans2.py
+++ ans2.py
@@ -1,14 +1,14 @@
 #function and nested loop
-num=float(input("Starting number of organism:"))
+a=float(input("Starting number of organism:"))
 # average daily percentage of the Amazon rainforest.
 per=float(input("Average daily percentage increase:"))
 # for getting output for number of days
 total=int(input("Enter the number of days to display the final data:"))
 print("day approximate","       ","population")
-c=num
-print(num)
+c=a
+print(a)
 for i in range (1,total):
   #for getting the increase value
- c =num+(num*(per/100))
+ c =a+(a*(per/100))
  print(i,"                ",c,"\n")
- num=c
+ a=c
